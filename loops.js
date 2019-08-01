9b1114f8de0f7f6f517d31d1b9110411723c39c3
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-08-01 18:00:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-function forLoop(array){
+function forLoop("array"){
   for (var i = 1; i < 25; i++) {
   if (i === 1) {console.log(array["I am " + i + " strange loop"])}
   else {console.log(array["I am " + i + "strange loops"])}
