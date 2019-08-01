008dc63401a9555f1fbc69b06fa9d169dead1c10
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-08-01 18:34:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,3 +13,10 @@ while (n > 0) {
 }
 return 'done'
 }
+function doWhileLoop(num) {function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+  do { console.log("I run once regardless")}
+  while (incrementVariable() < num)
+}
